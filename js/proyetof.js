ad2e52f7759070be32b55d1db3594cf0627bde5d
Michelle Delgado <94701337+Daam1Adam@users.noreply.github.com>
2022-05-03 00:24:13 -0300

    porcentaje

--- js/proyetof.js
+++ js/proyetof.js
@@ -1,9 +1,10 @@
 //Proyecto final - Calculadora
 
-const sumar = function (n1, n2) {return n1 + n2}
-const restar = function (n1, n2) {return n1 - n2}
-const multiplicar = function (n1, n2) {return n1 * n2}
-const dividir = function (n1, n2) {return n1 / n2}
+const sumar = function (n1, n2) {return n1 + n2};
+const restar = function (n1, n2) {return n1 - n2};
+const multiplicar = function (n1, n2) {return n1 * n2};
+const dividir = function (n1, n2) {return n1 / n2};
+const porcentaje = function (n1,n2) {return n1 * n2 / 100};
 
 let calculo = [];
 
@@ -13,7 +14,7 @@ let numero1 = parseFloat(prompt("Ingrese un número"));
 let numero2 = parseFloat(prompt("Ingrese un número"));
     calculo.push(numero2);
     console.log(calculo);
-let operacion = prompt("¿Qué operación quieres realizar? +,-,*,/");
+let operacion = prompt("¿Qué operación quieres realizar? +,-,*,/,%");
     calculo.push(operacion);
     console.log(calculo);
 if (operacion === "+") {
@@ -24,6 +25,8 @@ if (operacion === "+") {
     alert("Tu resultado es " + multiplicar(numero1,numero2));
 } else if (operacion === "/") {
     alert("Tu resultado es " + dividir(numero1,numero2));
+} else if (operacion === "%") {
+    alert("Tu resultado es " + porcentaje(numero1,numero2));
 } else {
     alert("Operacion no valida");
 }
