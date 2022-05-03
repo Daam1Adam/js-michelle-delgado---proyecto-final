fcddc21cda9ef74ef52a71fcde03592d9666ae39
Michelle Delgado <94701337+Daam1Adam@users.noreply.github.com>
2022-05-02 23:36:06 -0300

    calculadora - proyecto final

---
+++ js/proyetof.js
@@ -0,0 +1,6 @@
+//Proyecto final - Calculadora
+
+const sumar = function (numero1, numero2) {return numero1 + numero2}
+const restar = function (numero1, numero2) {return numero1 - numero2}
+const multiplicar = function (numero1, numero2) {return numero1 * numero2}
+const dividir = function (numero1, numero2) {return numero1 / numero2}
