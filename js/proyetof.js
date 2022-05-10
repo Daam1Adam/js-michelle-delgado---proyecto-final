e0402d95232129ca878b72a66570c1a76137dd77
Michelle Delgado <94701337+Daam1Adam@users.noreply.github.com>
2022-05-09 22:39:10 -0300

    entrega 0305

--- js/proyetof.js
+++ js/proyetof.js
@@ -1,42 +1,39 @@
 //Proyecto final - Calculadora
 
 //ENTREGA 1 03/05
-
-function sumar(n1, n2) { return n1 + n2; }
+let continuar;
+let calculos = [];
+const sumar = function (n1, n2) { return n1 + n2; }
 const restar = function (n1, n2) {return n1 - n2};
 const multiplicar = function (n1, n2) {return n1 * n2};
 const dividir = function (n1, n2) {return n1 / n2};
 const porcentaje = function (n1,n2) {return n1 * n2 / 100};
 
+do {
+    let numero1 = parseFloat(prompt("Ingrese un número"));
+    let numero2 = parseFloat(prompt("Ingrese un número"));
+    let operacion = prompt("¿Qué operación quieres realizar? +,-,*,/,%");
+    let calculo = {numero1,numero2,operacion};
+    calculos.push(calculo);
 
+    if (operacion === "+") {
+        alert("Tu resultado es " + sumar(numero1,numero2));
+    } else if (operacion === "-") {
+        alert("Tu resultado es " + restar(numero1,numero2));
+    } else if  (operacion === "*") {
+        alert("Tu resultado es " + multiplicar(numero1,numero2));
+    } else if (operacion === "/") {
+        alert("Tu resultado es " + dividir(numero1,numero2));
+    } else if (operacion === "%") {
+        alert("El " + numero1 + "% de " + numero2 + " es " + porcentaje(numero1,numero2));
+    } else {
+        alert("Operacion no valida");
+    }
+    continuar = confirm("¿Deseas hacer otra operación?")
+}while (continuar);
+console.log(calculos);
 
-let calculo = [];
-
-let numero1 = parseFloat(prompt("Ingrese un número"));
-    calculo.push(numero1);
-    console.log(calculo);
-let numero2 = parseFloat(prompt("Ingrese un número"));
-    calculo.push(numero2);
-    console.log(calculo);
-let operacion = prompt("¿Qué operación quieres realizar? +,-,*,/,%");
-    calculo.push(operacion);
-    console.log(calculo);
-if (operacion === "+") {
-    alert("Tu resultado es " + sumar(numero1,numero2));
-} else if (operacion === "-") {
-    alert("Tu resultado es " + restar(numero1,numero2));
-} else if  (operacion === "*") {
-    alert("Tu resultado es " + multiplicar(numero1,numero2));
-} else if (operacion === "/") {
-    alert("Tu resultado es " + dividir(numero1,numero2));
-} else if (operacion === "%") {
-    alert("El " + numero1 + "% de " + numero2 + " es " + porcentaje(numero1,numero2));
-} else {
-    alert("Operacion no valida");
-}
-
-/*
-const botonesNumeros = document.getElementsByClassName("numeros");
+/*const botonesNumeros = document.getElementsByClassName("numeros");
 console.log(botonesNumeros);
 const botonesOperaciones = document.getElementsByClassName("operaciones");
 const botonesEspeciales = document.getElementsByClassName("especiales");
@@ -48,5 +45,4 @@ botonesNumeros.forEach(function(botones) {
     boton.addEventListener('click', function(){
         alert(boton.innerText);
     })
-})
-*/
+})*/
